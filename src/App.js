ca8861dc496b38aa8f51565f0059800e80d24b37
Keidy Mejias <keidyalexandra1988@gmail.com>
2022-05-19 18:28:53 -0300

    Creacion CLI - Clase 1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Keidy Mejias - Comision 37735 </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto React Js
         </p>
         <a
           className="App-link"
