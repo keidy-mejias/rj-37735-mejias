556ccdb5a9b81ff74e72a0d7ea0336c5f1be48ca
Keidy Mejias <keidyalexandra1988@gmail.com>
2022-06-21 17:45:00 -0300

    Componentes II

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from './components/Navbar/Navbar.js'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer.js'
+import { ItemCount } from './components/ItemCount/ItemCount'
+import torta from './assets/imagen/torta.jpg'
 
 function App() {
 
   const usuario = {
     nombre: 'Keidy Mejias',
     cargo: 'Administrador'
+  }
+
+  const producto = {
+    imagen: torta ,
+    nombre: "Pastel",
+    precio: "2500",
+    cantidad: "5",
+    stock: true,
+    initial: 1
   }
 
   //const personal = {
@@ -22,6 +33,7 @@ function App() {
 
       <ItemListContainer titulo="Bienvenidos a la experiencia más dulce de tu vida" content="No te arrepentiras de nuestros sabores, prueba y verás"/>
       <ItemListContainer titulo={usuario.nombre} content={usuario.cargo}/>
+      <ItemCount nombre={ producto.nombre } imagen={ producto.imagen } precio={ producto.precio }/>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import './ItemCount.scss'
+
+//Agregare imagenes, nombre y valor o precio a las cards
+export const ItemCount = ( { imagen, nombre, precio }) => {
+
+    const [ contador, setContador] = useState(1)
+
+    const agregar = () => {
+        setContador ( contador + 1); 
+    }
+
+    const disminuir = () => {
+        if (contador <= 0) {
+            return
+        }
+
+        setContador ( contador - 1 )
+    }
+
+    return (
+
+        <section className="cards">
+
+            <div className="row">
+
+                <div className='card'>
+
+                    <h4 className="cardsTitle"> { nombre } </h4>
+                    <imagen className="imagenCard" src= { imagen } />
+                    <strong className="precioCard"> { precio } </strong>
+
+                    <div className='buttons'>
+                        <button className='btn btn-light' onClick={ disminuir }> - </button>
+
+                        <p className='cantProducto'> { contador } </p>
+
+                        <button className='btn btn-light' onClick={ agregar }> + </button>
+
+                    </div>
+
+                </div>
+
+                <hr/>
+
+            </div>
+            
+
+        </section>
+    )
+    }
+
+
+    //let contador = 1 metodo solo actualiza el numero en consola pero no muestra hay que pasarlo por estado
+    //let [contador, setContador] = useState(1)
+    //const incremento = () => {
+        //setContador( contador + 1 )
+    //} (Practica de la clase como usar un estado)
+
+/*<div className="contador my-5">
+            <h3>Counter</h3>
+            <hr/>
+
+            <button className='btn btn-primary' onClick={incremento}>Agregar</button>
+            <p> Resultado {contador} </p>
+            {/*<p> Fecha y hora click: {new Date().toLocaleDateString()} </p> Esto es si necesito usar fecha y hora*/
